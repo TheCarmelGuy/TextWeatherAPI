2f050bc52cb99c5036621b7ba25961e6d3d67dff
komelmerchant <komelm13@gmail.com>
2016-03-27 20:36:47 -0400

    Implementation of twilio-based text message service

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var http = require('http');
+var request = require('request');
 var twilio = require('twilio');
-var express = require('express');
-
 //var cronJob = require('cron').CronJob;
-//var client = twilio('ACee50e57ae84aba70052bf1e47b4b32d7','0351e3497ba5260b53a2fc486d6d8b3c');
+
+//set up twilio client 
+var client = twilio('ACee50e57ae84aba70052bf1e47b4b32d7','0351e3497ba5260b53a2fc486d6d8b3c');
 
 
 //api key to access weather data
@@ -12,11 +12,10 @@ var WEATHERAPPID = '2a2c8eb40b6adba813e3d7f575cdff50';
 //specifiy the url path that the program makes a request to 
 //var urlPath = '/data/2.5/weather?' + zipCode + ',' + countryCode + '&' + WEATHERAPPID;
 var url = 'http://api.openweathermap.org/data/2.5/weather/?';
+ 
 
-//setting path and opeiton s 
 
-var request = require('request');
-
+//set querystring parameters 
 var qs = {
 
     id:'5180225',
@@ -27,13 +26,18 @@ var qs = {
 };
 request({url:url , qs: qs} , function(err, res, body)  {
     
-    console.log(res.headers);//log the heades
     var info = JSON.parse(body);//data information on the weather at a particular location 
     console.log(info);
     var weather = info.main;
 
    var bodyOfMessage = `Good Morning Komel! The temperature today is ${weather.temp}. The high today is ${weather.temp_max} and the low today is ${weather.temp_min}. Enjoy your day today!`;
     console.log(bodyOfMessage);
-    
+
+    //send message to certain person
+    client.sendMessage({
+	to:'2158503673',
+	from:'+12672961860',
+	body: bodyOfMessage
+    }, null, true); 
 });
     
